330eba4964f40dd7fc4886f4105f769b7ac33a94
Greg Dobler <greg.dobler@gmail.com>
2019-09-13 03:07:18 +0000

    updated to include 2016 data

--- thystle/project.py
+++ thystle/project.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import time
 import hyss
 import numpy as np
 import pickle as pkl
 from scipy.interpolate import interp1d
 from sklearn.cluster import KMeans
+from scipy.ndimage.filters import gaussian_filter as gf
 
 # -- these should be turned into imports
-imps = ["utils.py", "select_pixels.py"]
+imps = ["utils.py", "select_pixels.py", "plotting.py"]
 for ii in imps:
     exec(open(ii).read())
 
@@ -77,5 +79,39 @@ elapsed_time(t0)
 # -- assign 2018 spectra to 2016 K-Means clusters
 labs18 = km16.predict(specs18)
 
+# -- create the luminosity image and plot
+imgL = cube.data.mean(0) * imgLcc
+lighting_tech3(labs18, km16, imgLcc, imgL.clip(0, 10)**0.5, aspect=0.43,
+               xlim=[450, imgLcc.shape[1]],
+               outfile="../output/lighting_technology_map_2018.png")
+
+# -- open the 2016 indices map
+imgLcc16 = np.load("../data/img_L_arr.npy")
+imgLcc16 = imgLcc16 > 0.5
+imgLcc16 = (gf(1.0 * imgLcc16, 1) > 0.25) * imgLcc16
+
+
+# -- get the 2016 image
+cname16 = os.path.join("..", "..", "data", "middleton",
+                       "night time vnir full frame",
+                       "full frame 20ms faster_VNIR.raw")
+cube16 = read_hyper_clean(cname16)
+imgL16 = cube16.data.mean(0) * imgLcc16
+
+# -- make the plot
+lighting_tech3(km16.labels_, km16, imgLcc16, imgL16.clip(0, 10)**0.5,
+               xlim=[0, 1090], ylim=[950, 185], aspect=0.35, 
+               outfile="../output/lighting_technology_map_2016.png")
+
 # -- total time
 elapsed_time(t00, "TOTAL ")
+
+
+
+
+
+# -- # -- # --
+# imgL = cube.data.mean() * imgLcc
+
+# imgL[185:950, :1090]
+# imgL18[:, 450:]
